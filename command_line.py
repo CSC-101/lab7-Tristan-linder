9f416f04d544e0d18858c43dff0e1f10f767e51c
Tristan-linder <tlinder@calpoly.edu>
2024-11-21 12:20:02 -0800

    Update command_line.py

--- command_line.py
+++ command_line.py
@@ -1,1 +1,14 @@
+import sys
+
+def main():
+    total = 0.0
+    for arg in sys.argv[1:]:
+        try:
+            total += float(arg)
+        except ValueError:
+            print(f"{arg} not valid, skip")
+    print(f"Total sum is {total}")
+
+if __name__ == "__main__":
+    main()
 
