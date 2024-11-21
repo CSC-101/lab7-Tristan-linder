373b7b286edc51413f5ef41432fefdba59849c36
Tristan-linder <tlinder@calpoly.edu>
2024-11-21 12:20:46 -0800

    Update convert_tests.py

--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,13 @@
-
+from typing import Optional
+def str_to_float(s) -> Optional[float]:
+    try:
+        s = float(s)
+        return s
+    except ValueError:
+        return None
+s = "34"
+print(str_to_float(s))
+print(type(float(s)))
+s2 = "WAH"
+print(str_to_float(s2))
+print(type(float(s)))
